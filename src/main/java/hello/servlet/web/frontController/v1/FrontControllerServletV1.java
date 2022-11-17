20d382f340b7557d3a0120a05c040dbcb7dcfefe
choivit <topvit5@hanmail.net>
2022-11-17 18:44:13 +0900

    221117 ::  프론트 컨트롤러 도입 - v1

---
+++ src/main/java/hello/servlet/web/frontController/v1/FrontControllerServletV1.java
@@ -0,0 +1,43 @@
+package hello.servlet.web.frontController.v1;
+
+import hello.servlet.web.frontController.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontController.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontController.v1.controller.MemberSaveControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    //미리 저장을 해놓고 부르면 꺼내서 쓸 수 있게 됨.
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        System.out.println("FrontControllerServletV1.service");
+        //상단의 로그 같은 경우는 console에다가 찍는 게 맞으나 여기서는 그냥 시스템에 찍음.
+
+        //해당 주소를 입력 하면 request.getRequestURI에 들어와서 뽑아내면
+        String requestURI = request.getRequestURI();
+
+        //미리 넣어뒀던 주소 값에서 동일 한 것을 찾아 연결해줌.
+        ControllerV1 controller = controllerMap.get(requestURI);
+
+        if(controller == null){
+
+        }
+    }
+}
