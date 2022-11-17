102fba2270e983c7672b4a5dad363ff59f25c41d
choivit <topvit5@hanmail.net>
2022-11-17 16:22:33 +0900

    221117 :: MVC 패턴 - 한계

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -28,4 +28,9 @@ public class MvcMemberListServlet extends HttpServlet {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
+    // 한계
+    // 포워드 중복. view로 이동하는 코드가 항상 중복. 공통화는 가능하지만 해당 메서드도 매번 호출해야 함. 이외에 ViewPath도 중복됨.
+//  // 사용하지 않는 코드도 있다. 사용할때도 사용을 하지 않을때도 있음.
+    // 공통처리가 어렵다는 문제
+    //
 }
